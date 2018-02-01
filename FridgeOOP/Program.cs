3f18a1f4f7ab485eae04b17cce3ebb8654d28645
danielvivacqua <drvivacqua@hotmail.com>
2018-02-01 11:15:27 -0500

    Created switch case, gave user options

--- FridgeOOP/Program.cs
+++ FridgeOOP/Program.cs
@@ -24,10 +24,22 @@ namespace FridgeOOP
                 Console.WriteLine("To take food, type 4.");
                 Console.WriteLine("To check food supplies, type 5.");
                 Console.WriteLine("To see all fridge details, type 6.");
-                Console.WriteLine("To restart, type 7.");
+                //Console.WriteLine("To restart, type 7.");
                 Console.WriteLine("To quit, type 8.");
 
                 userChoice = int.Parse(Console.ReadLine());
+
+                Fridge userFridge = new Fridge(false, "deluxe dispenser", 70, true);
+
+                switch(userChoice)
+                {
+                    case 1:
+                        userFridge.DispenseWater();
+                        break;
+                    case 2:
+                        Console.WriteLine(userFridge.ChangeBulbs());
+                        break;
+                }
 
             } while (userChoice != 8);
         }
